2f976ebaa2ea13b30cf247270c4590109cd4afa5
jovanprojev <jovanprojev@gmail.com>
2016-03-19 19:49:37 +0100

    add map using google API

--- app/scripts/services/addFactory.js
+++ app/scripts/services/addFactory.js
@@ -1,31 +1,41 @@
 MyApp
-    .factory('addFactory', ['$http','host',
-    function($http, host){
+    .factory('addFactory', ['$http', 'host',
+        function ($http, host) {
             var service = {};
 
-            service.addNotification = function(notification){
-              return $http({
-                method: "POST",
-                url: host +'rest/notifications',
-                data:notification
-              });
+            service.addNotification = function (notification) {
+                return $http({
+                    method: "POST",
+                    url: host + 'rest/notifications',
+                    data: notification
+                });
             },
 
-            service.addKorisnik = function(korisnik){
-              return $http({
-                method: "POST",
-                url: host +'rest/users',
-                data:korisnik
-              });
-            },
-            service.addRelacija = function(relation){
-              return $http({
-                method: "POST",
-                url: host +'rest/relations',
-                data:relation
-              });
-            }
+                service.addKorisnik = function (korisnik) {
+                    return $http({
+                        method: "POST",
+                        url: host + 'rest/users',
+                        data: korisnik
+                    });
+                },
+                service.addRelacija = function (relation) {
+                    return $http({
+                        method: "POST",
+                        url: host + 'rest/relations',
+                        data: relation
+                    });
+                },
+                service.addPins = function (notificationId, pins) {
+                    return $http({
+                        method: "POST",
+                        url: host + 'rest/pin/addPins',
+                        data: pins,
+                        params:{
+                            notificationId:notificationId
+                        }
+                    });
+                }
 
-            return service;                
+            return service;
         }
-        ]);
+    ]);
